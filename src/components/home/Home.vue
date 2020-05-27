8f036508e64844ea890e3823cdda43d81df15341
Stanislaw Cruz <techway.stanislaw@odontosystem.com.br>
2020-05-27 14:28:08 -0300

    Sincronizando a exclusão no lado do cliente.

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <h1 class="centralizado">{{ titulo }}</h1>
 
+    <p v-show="mensagem" class="centralizado">{{ mensagem }}</p>
     <input
       type="search"
       class="filtro"
@@ -12,7 +13,11 @@
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto in fotosComFiltro" :key="foto.url">
         <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva v-meu-transform:scale.animate="1.2" :url="foto.url" :titulo="foto.titulo" />
+          <imagem-responsiva
+            v-meu-transform:scale.animate="1.2"
+            :url="foto.url"
+            :titulo="foto.titulo"
+          />
           <meu-botao
             tipo="button"
             rotulo="REMOVER"
@@ -42,7 +47,8 @@ export default {
     return {
       titulo: 'Alurapic',
       fotos: [],
-      filtro: ''
+      filtro: '',
+      mensagem: ''
     };
   },
 
@@ -59,7 +65,15 @@ export default {
 
   methods: {
     remove(foto) {
-      console.log(`Removido ${foto.titulo}`);
+      this.$http.delete(`http://localhost:3000/v1/${foto._id}`)
+      .then(() => {
+          let indice = this.fotos.indexOf(foto);
+          this.fotos.splice(indice, 1);
+          this.mensagem = `Foto "${foto.titulo}" removida com sucesso`
+        }, err => {
+          console.log(err);
+          this.mensagem = 'Não foi possível remover a foto';
+      });
     }
   },
 
