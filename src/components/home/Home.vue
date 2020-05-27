3c027747599601408021d1f1742f6567036352f8
Stanislaw Cruz <techway.stanislaw@odontosystem.com.br>
2020-05-27 17:06:08 -0300

    Organizando recursos em serviços.

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -35,6 +35,7 @@
 import Painel from '../shared/painel/Painel.vue';
 import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
 import Botao from '../shared/botao/Botao.vue';
+import FotoService from '../../domain/foto/FotoService';
 
 export default {
   components: {
@@ -65,8 +66,8 @@ export default {
 
   methods: {
     remove(foto) {
-      this.resource
-        .delete({ id: foto._id })
+      this.service
+        .apaga(foto._id)
         .then(() => {
           let indice = this.fotos.indexOf(foto);
           this.fotos.splice(indice, 1);
@@ -80,10 +81,10 @@ export default {
   },
 
   created() {
-    this.resource = this.$resource('v1/fotos{/id}');
-    this.resource
-      .query()
-      .then(res => res.json())
+    this.service = new FotoService(this.$resource);
+
+    this.service
+      .lista()
       .then(fotos => (this.fotos = fotos), err => console.log(err));
   }
 };
